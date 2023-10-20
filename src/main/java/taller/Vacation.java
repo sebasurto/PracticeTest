bf4736ef15645eea3eb029badcc97dfa7f01f9ce
Sergio Basurto <35089326+sebasurto@users.noreply.github.com>
2023-10-19 22:23:23 -0500

    Some problems fixed

--- src/main/java/taller/Vacation.java
+++ src/main/java/taller/Vacation.java
@@ -1,72 +1,125 @@
 package taller;
 
 public class Vacation {
-	private static final int PARIS_COST = 500;
-    private static final int NEW_YORK_COST = 600;
-    private static final int BASE_COST = 1000;
-    private static final double GROUP_DISCOUNT_10_PERCENT = 0.90; 
-    private static final double GROUP_DISCOUNT_20_PERCENT = 0.80;
-    private static final int PENALTY_FEE = 200;
-    private static final int PROMOTION_DISCOUNT = 200;
-    private static final int MAX_GROUP_SIZE = 80;
-
-	String destination;
-	int travelers;
-	int daysOfVacation;
-	public Vacation(String destination, int travalers, int days) {
-		super();
-		this.destination = destination;
-		this.travelers = travalers;
-		this.daysOfVacation = days;
+/**
+ * Cost to Paris.
+ */
+ private static final int PARIS_COST = 500;
+ /**
+  * Cost to New York.
+  */
+ private static final int NEW_YORK_COST = 600;
+ /**
+  * Base Cost.
+  */
+ private static final int BASE_COST = 1000;
+ /**
+  * Cost to Paris.
+  */
+ private static final double GROUP_DISCOUNT_10_PERCENT = 0.90;
+ /**
+  * Cost to Paris.
+  */
+ private static final double GROUP_DISCOUNT_20_PERCENT = 0.80;
+ /**
+  * Cost to Paris.
+  */
+ private static final int PENALTY_FEE = 200;
+ /**
+  * Cost to Paris.
+  */
+ private static final int PROMOTION_DISCOUNT = 200;
+ /**
+  * Cost to Paris.
+  */
+ private static final int MAX_GROUP_SIZE = 80;
+ /**
+  *  Destination where do you travel.
+  *
+  */
+  private String destination;
+  /** Num of travelers. */
+  private int travelers;
+  /** Number of days of vacation. */
+  private int daysOfVacation;
+  /**
+  * Constructor for the Vacation class.
+  *
+  * @param destinationTr The destination of the vacation.
+  * @param numTrav The number of travelers.
+  * @param days The number of days for the vacation.
+  */
+public Vacation(final String destinationTr, final int numTrav, final int days) {
+  this.destination = destinationTr;
+  this.travelers = numTrav;
+  this.daysOfVacation = days;
+ }
+/**
+ * @return the destination to traveling.
+*/
+ public String getDestination() {
+  return destination;
+ }
+ /**
+  * @param destinationTr where you go.
+ */
+public void setDestination(final String destinationTr) {
+ this.destination = destinationTr;
+}
+public int getDaysOfVacation() {
+ return daysOfVacation;
+}
+/**
+ * @param days where you go.
+*/
+public void setDaysOfVacation(final int days) {
+ this.daysOfVacation = days;
+ }
+ public int getTravelers() {
+  return travelers;
+ }
+public void setTravelers(int travelers) {
+ this.travelers = travelers;
+}
+/**
+ * @return the cost to travel to Pais.
+*/
+ public static int getParisCost() {
+  return PARIS_COST;
+ }
+ /**
+  * @return the cost to travel to New Yrok.
+ */
+ public static int getNewYorkCost() {
+  return NEW_YORK_COST;
+ }
+ /**
+  * @return the base cost.
+ */
+ public static int getBaseCost() {
+  return BASE_COST;
+ }
+ public static double getGroupDiscount10Percent() {
+  return GROUP_DISCOUNT_10_PERCENT;
+ }
+ public static double getGroupDiscount20Percent() {
+  return GROUP_DISCOUNT_20_PERCENT;
+ }
+ public static int getPenaltyFee() {
+  return PENALTY_FEE;
+ }
+ public static int getPromotionDiscount() {
+  return PROMOTION_DISCOUNT;
+ }
+ public static int getMaxGroupSize() {
+ return MAX_GROUP_SIZE;
+ }
+@Override
+ public String toString() {
+  return "Vacation [destination=" + destination + ", travalers=" + travelers + ", days=" + daysOfVacation
++ "]";
 	}
-	public String getDestination() {
-		return destination;
-	}
-	public void setDestination(String destination) {
-		this.destination = destination;
-	}
-	public int getDaysOfVacation() {
-		return daysOfVacation;
-	}
-	public void setDaysOfVacation(int daysOfVacation) {
-		this.daysOfVacation = daysOfVacation;
-	}
-	public int getTravelers() {
-		return travelers;
-	}
-	public void setTravelers(int travelers) {
-		this.travelers = travelers;
-	}
-	public static int getParisCost() {
-		return PARIS_COST;
-	}
-	public static int getNewYorkCost() {
-		return NEW_YORK_COST;
-	}
-	public static int getBaseCost() {
-		return BASE_COST;
-	}
-	public static double getGroupDiscount10Percent() {
-		return GROUP_DISCOUNT_10_PERCENT;
-	}
-	public static double getGroupDiscount20Percent() {
-		return GROUP_DISCOUNT_20_PERCENT;
-	}
-	public static int getPenaltyFee() {
-		return PENALTY_FEE;
-	}
-	public static int getPromotionDiscount() {
-		return PROMOTION_DISCOUNT;
-	}
-	public static int getMaxGroupSize() {
-		return MAX_GROUP_SIZE;
-	}
-	@Override
-	public String toString() {
-		return "Vacation [destination=" + destination + ", travalers=" + travelers + ", days=" + daysOfVacation
-				+ "]";
-	}
-    public double calculateTotalCost() {
+public double calculateTotalCost() {
     	if (isValidInput()) {
             double totalCost = BASE_COST;
 	        if (destination.equalsIgnoreCase("Paris")) {
@@ -74,29 +127,24 @@ public class Vacation {
 	        } else if (destination.equalsIgnoreCase("New York City")) {
 	            totalCost += NEW_YORK_COST;
 	        }
-	
 	        if (travelers > 4 && travelers < 10) {
 	            totalCost *= GROUP_DISCOUNT_10_PERCENT;
 	        } else if (travelers > 10) {
 	            totalCost *= GROUP_DISCOUNT_20_PERCENT;
 	        }
-	
 	        totalCost *= daysOfVacation;
-	        
 	        if (daysOfVacation < 7) {
 	            totalCost += PENALTY_FEE;
 	        }
-	
 	        if (daysOfVacation > 30 || daysOfVacation == 2) {
 	            totalCost -= PROMOTION_DISCOUNT;
 	        }
 	        return totalCost;
     	} else {
-            return -1; 
+            return -1;
         }
     }
-    private boolean isValidInput() {
-        return (travelers >= 1 && daysOfVacation >= 1 && travelers <= MAX_GROUP_SIZE);
+ private boolean isValidInput() {
+ return (travelers >= 1 && daysOfVacation >= 1 && travelers <= MAX_GROUP_SIZE);
     }
-	
 }
